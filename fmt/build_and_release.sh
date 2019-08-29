6140ef1cbffb985b8e87b8a366249f7481dd6e1d
aresyang <yangrui.li@mobvista.com>
2019-08-29 22:22:32 +0800

    add fmt

---
+++ fmt/build_and_release.sh
@@ -0,0 +1,46 @@
+function select_compiler() {
+ #https://stackoverflow.com/questions/17275348/how-to-specify-new-gcc-path-for-cmake
+ devtool=/opt/rh/devtoolset-7/root/usr/
+ export CC=$devtool/bin/gcc
+ export CXX=$devtool/bin/g++
+}
+function cmake_build() {
+    source_dir=$1
+    build=$source_dir/build
+    if [[ ! -d $build ]];then
+        mkdir $build
+    fi
+    cd $build
+	if [[ -f CMakeCache.txt ]];then
+        rm CMakeCache.txt
+    fi
+    select_compiler
+    if [[ 0 -eq 0 ]];then
+        cmake3 -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+                        -DBUILD_SHARED_LIBS=OFF \
+                        -DFMT_TEST=OFF \
+                        -DFMT_DOC=OFF \
+                        ..
+        ninja
+    fi
+    cd -
+}
+function deploy() {
+    source_dir=$1
+    deploy=$2
+    cp $source_dir/include/fmt/*h -a $deploy
+    if [[ ! -d $deploy/bin ]];then
+        mkdir $deploy/bin
+    fi
+    cp $source_dir/build/bin/* $deploy/bin/
+
+    if [[ ! -d $deploy/lib64_release ]];then
+        mkdir $deploy/lib64_release
+    fi
+    cp $source_dir/build/libfmt.a $deploy/lib64_release
+}
+source_dir=$1
+cmake_build $source_dir
+if [[ $? -eq 0 ]];then
+    deploy $source_dir $(pwd)
+fi
