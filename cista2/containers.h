7a773161ea54edda8891433a2bdfa985d1ebaf21
aresyang <yangrui.li@mobvista.com>
2019-09-26 14:00:46 +0800

    remove cista2

--- cista2/containers.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "cista/containers/array.h"
-#include "cista/containers/hash_map.h"
-#include "cista/containers/hash_set.h"
-#include "cista/containers/string.h"
-#include "cista/containers/unique_ptr.h"
-#include "cista/containers/vector.h"
